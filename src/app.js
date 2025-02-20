e182976a38a37c8da4c8bed3134b58b670e556ad
Madg26 <manux_852@hotmail.com>
2025-02-20 00:54:09 +0000

    [Cree la carta de poker aleatoria]

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@ import "bootstrap";
 import "./style.css";
 
 
+
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function randomCard(array) {
+  let numberRandom = Math.floor(Math.random() * array.length);
+  return array[numberRandom]
+  
+}
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let numbers = ["A", "K", "Q", "J", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let simbols = ["♦", "♥", "♠", "♣"];
+  let numbersRandom = randomCard(numbers)
+  let simbolsRandom = randomCard(simbols)
+  document.getElementById("number").innerText = numbersRandom;
+  document.getElementsByClassName("card-heart")[0].innerHTML = simbolsRandom
+  document.getElementsByClassName("card-heart")[1].innerHTML = simbolsRandom
+}
+
