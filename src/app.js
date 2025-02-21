16d2601d2d591f7071fad3f982d5bc8a2624140e
Madg26 <manux_852@hotmail.com>
2025-02-21 01:19:36 +0000

    [Add my card random]

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import "./assets/img/4geeks.ico";
 function randomCard(array) {
   let numberRandom = Math.floor(Math.random() * array.length);
   return array[numberRandom]
-  
+
 }
 
 window.onload = function () {
@@ -19,7 +19,16 @@ window.onload = function () {
   let numbersRandom = randomCard(numbers)
   let simbolsRandom = randomCard(simbols)
   document.getElementById("number").innerText = numbersRandom;
-  document.getElementsByClassName("card-heart")[0].innerHTML = simbolsRandom
-  document.getElementsByClassName("card-heart")[1].innerHTML = simbolsRandom
-}
+  document.getElementsByClassName("card-heart")[0].innerHTML = simbolsRandom;
+  document.getElementsByClassName("card-heart")[1].innerHTML = simbolsRandom;
+  let colorRandomTop = document.getElementsByClassName("card-heart")[0];
+  let colorRandomBottom = document.getElementsByClassName("card-heart")[1];
+  if (simbolsRandom === "♠" || simbolsRandom === "♣") {
+    colorRandomTop.style.color = 'black';
+    colorRandomBottom.style.color = 'black';
+  } else {
+    colorRandomTop.style.color = 'red';
+    colorRandomBottom.style.color = 'red';
+  }
 
+}
